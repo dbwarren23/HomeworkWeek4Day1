25552fd57f52d83610fe617530a83fe6cfa1d62b
davidwarren <dbwarren23@gmail.com>
2022-04-10 22:47:24 -0400

    commit error test project

---
+++ src/testproject.java
@@ -0,0 +1,38 @@
+
+public class testproject {
+
+	Public static void main(String[] args) {
+		System.out.println();
+
+	}
+	
+	public static void main[String[] args] {
+		System.out.println();
+
+	}
+	
+	public static void main(String[] args) {
+		Int x = 10;
+		System.out.println(x);
+
+	}
+	
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println(X);
+
+	}
+	
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println(x)
+
+	}
+	
+	public static void main(String[] args) {
+		String x = 10;
+		System.out.println(x+1);
+
+	}
+
+}
